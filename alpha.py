e65ea370d2de8f6058d7d1ae6e13d92097d0726b
Gaius Nale <gaiusnale@gmail.com>
2024-06-30 22:58:45 +0100

    will work on the env properties later

--- alpha.py
+++ alpha.py
@@ -1,6 +1,18 @@
 import os 
 import telebot 
+from keysus import key
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN = key
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'hello'])
+def welcome(message):
+    bot.reply_to(message, "Hello there")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_messages(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
+
