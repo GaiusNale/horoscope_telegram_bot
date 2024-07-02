1aa4e368f44ab4aa64701799d1f8698f22e0e026
Gaius Nale <gaiusnale@gmail.com>
2024-07-02 16:04:02 +0100

    creating functions to pick signs and days

--- alpha.py
+++ alpha.py
@@ -19,8 +19,16 @@ def get_daily_horoscope(sign: str, day: str) -> dict:
 
     return response.json()
 
-@bot.message
+@bot.message_handler(commands= ['horoscope'])
 
+def sign_picker(message):
+    text = "What's your Zodiac sign?"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode= "Markdown")
+    bot.register_next_step_handler(sent_msg, day_picker)
+
+def day_picker(message):
+    sign = message.text 
+    text = "What day would you like to find out about?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date with the format YYYY-MM-DD."
 
 
 # @bot.message_handler(commands=['start', 'hello'])
