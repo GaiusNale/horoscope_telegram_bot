e7bc530493a56664e3ed99c06ac345994daa6787
Gaius Nale <gaiusnale@gmail.com>
2024-07-02 16:15:22 +0100

    finished the function

--- alpha.py
+++ alpha.py
@@ -28,7 +28,13 @@ def sign_picker(message):
 
 def day_picker(message):
     sign = message.text 
-    text = "What day would you like to find out about?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date with the format YYYY-MM-DD."
+    text = "What day would you like to find out about?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date with the format YYYY-MM-DD"
+    sent_msg = bot.send_message (
+        message.chat.id, text, parse_mode= "Markdown"
+    )
+    bot.register_next_step_handler(
+        sent_msg, fetch_horoscope, sign_picker
+    )
 
 
 # @bot.message_handler(commands=['start', 'hello'])
